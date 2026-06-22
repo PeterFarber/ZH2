f9fc18d463c9512df214686e3e4fc3b14bcdee40
Peter Farber <farberpete@gmail.com>
2026-06-22 14:40:53 -0400

    Cover Blender PBR bake spec details

--- tools/blender/pbr_bake_export/tests/test_bake.py
+++ tools/blender/pbr_bake_export/tests/test_bake.py
@@ -6,11 +6,16 @@ from tools.blender.pbr_bake_export.addon.bake import bake_map_specs
 class BakeTests(unittest.TestCase):
     def test_bake_map_specs_include_required_pbr_outputs(self):
         specs = bake_map_specs()
-        self.assertEqual([spec.role for spec in specs], ["basecolor", "normal", "roughness", "metallic", "ao"])
-        self.assertEqual(specs[0].blender_bake_type, "DIFFUSE")
-        self.assertEqual(specs[0].color_space, "sRGB")
-        self.assertEqual(specs[1].blender_bake_type, "NORMAL")
-        self.assertEqual(specs[1].color_space, "Non-Color")
+        self.assertEqual(
+            [(spec.role, spec.blender_bake_type, spec.color_space) for spec in specs],
+            [
+                ("basecolor", "DIFFUSE", "sRGB"),
+                ("normal", "NORMAL", "Non-Color"),
+                ("roughness", "ROUGHNESS", "Non-Color"),
+                ("metallic", "EMIT", "Non-Color"),
+                ("ao", "AO", "Non-Color"),
+            ],
+        )
 
     def test_every_bake_map_has_png_filename_suffix(self):
         for spec in bake_map_specs():
